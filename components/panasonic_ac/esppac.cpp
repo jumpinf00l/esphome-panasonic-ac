e465b2b1651b1450586bc03bce656d2aef8ba60f
jumpinf00l <45162465+jumpinf00l@users.noreply.github.com>
2025-07-03 08:34:43 +0800

    Update esppac.cpp
    Testing fan modes and preset modes

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -23,12 +23,14 @@ climate::ClimateTraits PanasonicAC::traits() {
   
   traits.set_supported_custom_fan_modes({"Automatic", "1", "2", "3", "4", "5"});
 
-  traits.set_supported_fan_modes({climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_QUIET, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH, climate::CLIMATE_FAN_FOCUS});
+  traits.set_supported_fan_modes({climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_DIFFUSE, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH, climate::CLIMATE_FAN_FOCUS});
   
 /*  traits.set_supported_swing_modes({climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH,
                                     climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL}); */
   
   traits.set_supported_custom_presets({"Normal", "Powerful", "Quiet"});
+
+  traits.set_supported_presets({climate::CLIMATE_PRESET_BOOST, climate::CLIMATE_PRESET_SLEEP})
   
   return traits;
 }
