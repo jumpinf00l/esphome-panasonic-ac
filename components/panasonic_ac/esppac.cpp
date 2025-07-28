c0c4450311ab6d9b948fb27f52c499a49bbf39dc
jumpinf00l <45162465+jumpinf00l@users.noreply.github.com>
2025-07-28 08:28:09 +0800

    Update esppac.cpp 828

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -18,8 +18,23 @@ climate::ClimateTraits PanasonicAC::traits() {
   traits.set_visual_temperature_step(TEMPERATURE_STEP);
   traits.set_supported_modes({climate::CLIMATE_MODE_OFF, climate::CLIMATE_MODE_HEAT_COOL, climate::CLIMATE_MODE_COOL, climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_FAN_ONLY, climate::CLIMATE_MODE_DRY});
   traits.set_supported_fan_modes({climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_QUIET, climate::CLIMATE_FAN_DIFFUSE, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH, climate::CLIMATE_FAN_FOCUS});
-  traits.set_supported_swing_modes({climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_BOTH, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL});
   traits.set_supported_presets({climate::CLIMATE_PRESET_NONE, climate::CLIMATE_PRESET_BOOST, climate::CLIMATE_PRESET_ECO});
+
+  // Dynamically set supported swing modes based on enable flags
+  std::set<climate::ClimateSwingMode> supported_swing_modes;
+  if (this->vertical_swing_enable_ || this->horizontal_swing_enable_) {
+      supported_swing_modes.insert(climate::CLIMATE_SWING_OFF);
+  }
+  if (this->vertical_swing_enable_) {
+      supported_swing_modes.insert(climate::CLIMATE_SWING_VERTICAL);
+  }
+  if (this->horizontal_swing_enable_) {
+      supported_swing_modes.insert(climate::CLIMATE_SWING_HORIZONTAL);
+  }
+  if (this->vertical_swing_enable_ && this->horizontal_swing_enable_) {
+      supported_swing_modes.insert(climate::CLIMATE_SWING_BOTH);
+  }
+  traits.set_supported_swing_modes(supported_swing_modes);
   
   return traits;
 }
